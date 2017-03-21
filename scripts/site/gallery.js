f8993c65475e66a27415c547dd6a38c62c176467
gregorybchris <gregorybchris@gmail.com>
2017-03-20 21:40:27 -0700

    Added photography

---
+++ scripts/site/gallery.js
@@ -0,0 +1,15 @@
+// var dir = "../../images/photography";
+// var extension = ".jpg";
+// $.ajax({
+//     url: dir,
+//     success: function (data) {
+//         $(data).find("a:contains(" + extension + ")").each(function () {
+//             var filename = this.href.replace(window.location.host, "").replace("http://", "");
+//             $(".photography").append("<img src='" + dir + filename + "'>");
+//         });
+//     }
+// });
+
+// <a class="photography-image-link" href="./images/photography/IMAG0051.jpg" data-lightbox="photos" data-title="Caption">
+//     <img class="photography-image" src="./images/photography/IMAG0051.jpg">
+// </a>
